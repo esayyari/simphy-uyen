84fd20ffdd90c1d574a8f5881f1c232c8376c92c
uym2 <ceciliamaitouyen@gmail.com>
2017-01-04 22:25:48 -0800

    minor fix

--- rooting-uyen/evalTrees_br2trueroot.sh
+++ rooting-uyen/evalTrees_br2trueroot.sh
@@ -8,7 +8,7 @@ nw_topology $truetrees > true_topology
 
 while read info && read -u 3 truetree; do 
 	echo $truetree > tree_temp
-	./br2trueroot.sh tree_temp $info
+	br2trueroot.sh tree_temp $info
 done < info_temp 3< true_topology | numlist -avg
 
 rm info_temp tree_temp true_topology
