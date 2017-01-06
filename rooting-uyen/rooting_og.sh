6a0b16292d5fa84809500fd92f1ccd793059c51b
uym2 <ceciliamaitouyen@gmail.com>
2017-01-06 15:24:44 -0800

    not evaluate trees in rooting_og.sh

--- rooting-uyen/rooting_og.sh
+++ rooting-uyen/rooting_og.sh
@@ -12,9 +12,9 @@ nw_prune $tree\_OG 0 > $tree\_ogrm_OG
 nw_topology $tree\_labeled > $tree\_topology
 
 # compute "no. branches" distances
-nw_distance $tree\_topology 0 | numlist -sub1 | numlist -avg > br2trueRoot_OG.txt
-cp br2trueRoot_OG.txt br2trueRoot_ogrm_OG.txt # the same root no matter if you include the outgroup or not
+#nw_distance $tree\_topology 0 | numlist -sub1 | numlist -avg > br2trueRoot_OG.txt
+#cp br2trueRoot_OG.txt br2trueRoot_ogrm_OG.txt # the same root no matter if you include the outgroup or not
 
 # compute triplet distances
-evalTrees_dtrpl.sh $tree\_labeled $tree\_OG > dtrpl_OG.txt
-evalTrees_dtrpl.sh $tree\_ogrm $tree\_ogrm_OG > dtrpl_ogrm_OG.txt
+#evalTrees_dtrpl.sh $tree\_labeled $tree\_OG > dtrpl_OG.txt
+#evalTrees_dtrpl.sh $tree\_ogrm $tree\_ogrm_OG > dtrpl_ogrm_OG.txt
