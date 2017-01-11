3938c42ed7493cced6ba24662451368ae1bf47b6
uym2 <ceciliamaitouyen@gmail.com>
2017-01-10 21:46:18 -0800

    Fixed in OG_MV_adjust.sh

--- rooting-uyen/OG_MV_adjust.py
+++ rooting-uyen/OG_MV_adjust.py
@@ -7,11 +7,11 @@ theta = 1.75
 with open(infile,'r') as f:
 	for line in f:
 		std_OG,std_MV,dtrpl_OG,dtrpl_MV = [ float(x) for x in line.split() ]
-	if std_MV == 0:
-		print(dtrpl_MV)
-	else:
-		r = std_OG/std_MV
-		if r > theta:
-			print dtrpl_MV
+		if std_MV == 0:
+			print(dtrpl_MV)
 		else:
-			print dtrpl_OG
+			r = std_OG/std_MV
+			if r > theta:
+				print(dtrpl_MV)
+			else:
+				print(dtrpl_OG)
