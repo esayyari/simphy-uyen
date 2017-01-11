51f778908dbe1d2f0722faafba8b0b55f3d0be59
uym2 <ceciliamaitouyen@gmail.com>
2017-01-10 21:39:44 -0800

    OG_MV_adjust.py

---
+++ rooting-uyen/OG_MV_adjust.py
@@ -0,0 +1,16 @@
+import sys
+
+infile=sys.argv[1]
+
+theta = 1.75
+with open(infile,'r') as f:
+	for line in f:
+		std_OG,std_MV,dtrpl_OG,dtrpl_MV = [ int(x) for x in line.split() ]
+	if std_MV == 0:
+		print(dtrpl_MV)
+	else:
+		r = std_OG/std_MV
+		if r > theta:
+			print dtrpl_MV
+		else:
+			print dtrpl_OG
