1a690b1bee3f3b88f72ead5148f6c4e7c334a55f
uym2 <ceciliamaitouyen@gmail.com>
2017-01-10 16:25:40 -0800

    Simplify run_mpest.sh

--- rooting-uyen/run_mpest.sh
+++ rooting-uyen/run_mpest.sh
@@ -1,7 +1,7 @@
 # ! /bin/bash
 
 fn=$1
-tmp=`mktemp "control-$fn-$x-XXXXX"`
+tmp=`mktemp "control-$fn-XXXXX"`
 echo "$fn
 0
 `expr $RANDOM$RANDOM % 10000000`
@@ -10,26 +10,7 @@ echo "$fn
 0
 "> $tmp
 
-mpest $tmp 1>$fn.$x.mpest.out
-
-test $? == 1 || exit 1
+mpest $tmp 1>$fn.mpest.out
 
 mv $fn.tre $fn.mpest.all
-grep "tree mpest" $fn.mpest.all
-test $? == 0 || exit 1
-
-python -c ' 
-import os
-import sys
-import dendropy
-
-src_fpath = os.path.expanduser(os.path.expandvars("'$fn.mpest.all'"))
-if not os.path.exists(src_fpath):
-    sys.stderr.write("Not found: %s" % src_fpath) 
-    sys.exit(1) 
-
-    dest_fpath = os.path.expanduser("'$fn.mpest.tre'")
-    trees = dendropy.TreeList.get_from_path(src_fpath, "nexus")
-    trees[-1].write_to_path(dest_fpath, "newick",write_rooting=False)'
-
-test $? == 0 || exit 1
+grep "tree mpest" $fn.mpest.all | awk -F '= ' '{print $2;}' > $fn.mpest.tre
