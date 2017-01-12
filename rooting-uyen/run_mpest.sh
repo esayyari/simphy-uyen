916fe5e364e9a3f79dbf48f924e2fbb1667c5ef2
uym2 <ceciliamaitouyen@gmail.com>
2017-01-11 17:06:49 -0800

    run_mpest.sh

--- rooting-uyen/run_mpest.sh
+++ rooting-uyen/run_mpest.sh
@@ -1,11 +1,13 @@
 # ! /bin/bash
+# Note: only work with the convention of my experiments for the project
+# species taxa and gene taxa are the same. Each species has only 1 taxon
 
 fn=$1
 tmp=`mktemp "control-$fn-XXXXX"`
 echo "$fn
 0
 `expr $RANDOM$RANDOM % 10000000`
-`cat $1|wc -l` `cat $2|wc -l` 
+`cat $1|wc -l` `cat $2|wc -l`
 `cat $2`
 0
 "> $tmp
@@ -13,4 +15,20 @@ echo "$fn
 mpest $tmp 1>$fn.mpest.out
 
 mv $fn.tre $fn.mpest.all
-grep "tree mpest" $fn.mpest.all | awk -F '= ' '{print $2;}' > $fn.mpest.tre
+#grep "tree mpest" $fn.mpest.all | awk -F '= ' '{print $2;}' > $fn.mpest.tre
+
+python -c '
+import os
+import sys
+import dendropy
+
+src_fpath = os.path.expanduser(os.path.expandvars("'$fn.mpest.all'"))
+if not os.path.exists(src_fpath):
+    sys.stderr.write("Not found: %s" % src_fpath) 
+    sys.exit(1) 
+
+dest_fpath = os.path.expanduser("'$fn.mpest.tre'")
+trees = dendropy.TreeList.get_from_path(src_fpath, "nexus")
+trees[-1].write_to_path(dest_fpath, "newick",suppress_rooting=True)'
+
+test $? == 0 || exit 1
