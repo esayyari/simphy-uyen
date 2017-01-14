0c3fa06ffe18d58bf77fe1808d5af6bfdd1e043f
uym2 <ceciliamaitouyen@gmail.com>
2017-01-13 17:14:01 -0800

    fix_tree_for_mpest.sh : remove polytomies

--- rooting-uyen/fix_tree_for_mpest.sh
+++ rooting-uyen/fix_tree_for_mpest.sh
@@ -2,4 +2,7 @@
 
 intrees=$1
 
+# add "S" to the begining of each taxon and remove all internal labels
 cat $intrees | sed -e 's/\([,(]\)\([0-9][0-9]*\)/\1S\2/g' | sed -e 's/I\([0-9][0-9]*\)//g'
+arb_resolve_polytomies.py $intrees
+
