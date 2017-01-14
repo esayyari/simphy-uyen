2aeb405c9bd8628a7ca6e123ff5d541777dfd48f
uym2 <ceciliamaitouyen@gmail.com>
2017-01-13 17:38:19 -0800

    fix_tree_for_mpest.sh

--- rooting-uyen/fix_tree_for_mpest.sh
+++ rooting-uyen/fix_tree_for_mpest.sh
@@ -1,8 +1,11 @@
 # !/bin/bash
 
 intrees=$1
+temp=`mktemp`
 
-# add "S" to the begining of each taxon and remove all internal labels
-cat $intrees | sed -e 's/\([,(]\)\([0-9][0-9]*\)/\1S\2/g' | sed -e 's/I\([0-9][0-9]*\)//g'
+# resolve polytomies
 arb_resolve_polytomies.py $intrees
 
+# add "S" to the begining of each taxon and remove all internal labels
+cat $intrees.resolved | sed -e 's/\([,(]\)\([0-9][0-9]*\)/\1S\2/g' |  sed -e 's/I\([0-9][0-9]*\)//g' > $intrees\_fixed
+rm $intrees.resolved
