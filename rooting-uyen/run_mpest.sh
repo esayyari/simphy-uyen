b40fe1d28a3c6cac913f0b03e0307b0ac8cb99e3
uym2 <ceciliamaitouyen@gmail.com>
2017-01-14 12:32:45 -0800

    run_mpest.sh: calls dendropy to convert nexus file to newick

--- rooting-uyen/run_mpest.sh
+++ rooting-uyen/run_mpest.sh
@@ -15,6 +15,23 @@ echo "$fn
 mpest $tmp 1>$fn.mpest.out
 
 mv $fn.tre $fn.mpest.all
-grep "tree mpest" $fn.mpest.all | awk -F '= ' '{print $2;}' > $fn.mpest.tre
+#grep "tree mpest" $fn.mpest.all | awk -F '= ' '{print $2;}' > $fn.mpest.tre
+
+python -c '
+import os
+import sys
+import dendropy
+ 
+src_fpath = os.path.expanduser(os.path.expandvars("'$fn.mpest.all'"))
+if not os.path.exists(src_fpath):
+    sys.stderr.write("Not found: %s" % src_fpath) 
+    sys.exit(1)     
+ 
+dest_fpath = os.path.expanduser("'$fn.mpest.tre'")
+
+trees = dendropy.TreeList.get_from_path(src_fpath, "nexus")
+trees[-1].write_to_path(dest_fpath, "newick",suppress_rooting=True)'
+
+
 
 test $? == 0 || exit 1
