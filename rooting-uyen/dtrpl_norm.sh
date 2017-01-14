28c50c5f2231a3e4be7c96a8b73aee6aeeff0e1c
uym2 <ceciliamaitouyen@gmail.com>
2017-01-14 11:14:17 -0800

    dtrpl: call fancy version

--- rooting-uyen/dtrpl_norm.sh
+++ rooting-uyen/dtrpl_norm.sh
@@ -7,6 +7,6 @@ perl -pi -e 'chomp if eof' $tt
 perl -pi -e 'chomp if eof' $et
 nleaves=`nw_stats $tt | grep "leaves" | cut -f2`
 ntrpl=`triplet_count.sh $nleaves`
-ntrpl_error=`quart_bin calcTripDist $tt $et`
+ntrpl_error=`trip_bin fancy calcTripDist $tt $et`
 
 echo $ntrpl_error | numlist -div`echo $ntrpl`
