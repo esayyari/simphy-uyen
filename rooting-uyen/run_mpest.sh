ce5525f210afd8edb6b840bf57f426fa35fd2387
uym2 <ceciliamaitouyen@gmail.com>
2017-01-18 00:48:51 -0800

    even more issues with MP-est

--- rooting-uyen/run_mpest.sh
+++ rooting-uyen/run_mpest.sh
@@ -4,8 +4,12 @@ fn=$1
 ctrl=`mktemp "control-$fn-XXXXX"`
 map=`mktemp "map-$fn-XXXXX"`
 species=`mktemp "species-$fn-XXXXX"`
-cat $fn | sed -e "s/:[0-9.e-]*//g" -e "s/)[0-9.e-]*/)/g" -e "s/[(,);]/ /g" -e "s/ /\n/g"|sort|uniq|tail -n+2 > $species 
-cat $species | sed -e "s/^\(.*\)$/\1 1 \1/g" > $map
+#cat $fn | sed -e "s/:[0-9.e-]*//g" -e "s/)[0-9.e-]*/)/g" -e "s/[(,);]/ /g" -e "s/ /\n/g"|sort|uniq|tail -n +3 > $species 
+head -n 1 $fn | nw_labels -I - > $species
+#cat $species | sed -e "s/^\(.*\)$/\1 1 \1/g" > $map
+while read line; do
+	echo $line 1 $line
+done < $species > $map
 
 echo "$fn
 0
@@ -22,4 +26,4 @@ tmp_tre=`mktemp "tre-$fn-XXXXX"`
 grep "tree mpest" $fn.mpest.all | awk -F '= ' '{print $2;}' > $tmp_tre
 fix_mpest_output.py $tmp_tre $species $fn.mpest.tre
 
-rm $ctrl $tmp_tre $map $species
+#rm $ctrl $tmp_tre $map $species
