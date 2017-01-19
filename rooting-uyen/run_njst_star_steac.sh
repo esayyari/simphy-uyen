f2c723d12ef7fb96f01ee5151b1a8c9cc88ef8b9
uym2 <ceciliamaitouyen@gmail.com>
2017-01-18 22:05:52 -0800

    run_njst_star_steac.sh

--- rooting-uyen/run_njst_star_steac.sh
+++ rooting-uyen/run_njst_star_steac.sh
@@ -7,10 +7,21 @@ s_tree=$2 # species tree (output)
 method=$3
 
 # create map file (map species names and genetrees' taxa)
-tmp_map=`mktemp map-XXXXX`
-cat $genetrees |sed -e "s/:[^),]*//g" -e "s/)[0-9.]*//g" -e "s/[(,);]/ /g" -e 's/ /\'$'\n''/g' |sort|uniq|tail -n+2|sed "s/\(.*\)\_.*\_.*$/& s\1/" > $tmp_map
+#tmp_map=`mktemp map-XXXXX`
+#cat $genetrees |sed -e "s/:[^),]*//g" -e "s/)[0-9.]*//g" -e "s/[(,);]/ /g" -e 's/ /\'$'\n''/g' |sort|uniq|tail -n+2|sed "s/\(.*\)\_.*\_.*$/& s\1/" > $tmp_map
+
+species=`mktemp "species-XXXXX"`
+head -n 1 $genetrees | nw_labels -I - | sort | tail -n +2 > $species
+
+map=`mktemp "map-XXXXX"`
+while read line; do
+        #echo $line > $tmp
+        #echo `awk -F 'S' '{print $2;}' $tmp'` 1 $line
+        echo $line $line
+done < $species > $map
+
 
 #call Rscript (the main dish is here!)
-Rscript steac_star_njst.r $method $genetrees $tmp_map "0_0_0" $s_tree
+Rscript steac_star_njst.r $method $genetrees $map "0_0_0" $s_tree
 
-rm $tmp_map
+rm $species $map
