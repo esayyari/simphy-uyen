ecd4acfdcf96f72f887a41ca04234a840d4d834c
uym2 <ceciliamaitouyen@gmail.com>
2017-01-21 22:09:38 -0800

    njst & star

--- rooting-uyen/run_njst_star_steac.sh
+++ rooting-uyen/run_njst_star_steac.sh
@@ -2,26 +2,35 @@
 
 #module load R/3.2.2_1
 
-genetrees=$1
-s_tree=$2 # species tree (output)
-method=$3
+method=$1
+genetrees=$2
+s_tree=$3 # species tree (output)
+
+gtre_fixed=`mktemp "tre-XXXXX"`
+# fix input trees (taxon name starts with 'S' and all polotomies resolved)
+fix_tree_for_mpest.sh $genetrees $gtre_fixed 
+
+gtre_dmog=`mktemp "tre-XXXXX"`
+add_dmog_w_lengths.sh $gtre_fixed D00 $gtre_dmog 
 
 # create map file (map species names and genetrees' taxa)
-#tmp_map=`mktemp map-XXXXX`
-#cat $genetrees |sed -e "s/:[^),]*//g" -e "s/)[0-9.]*//g" -e "s/[(,);]/ /g" -e 's/ /\'$'\n''/g' |sort|uniq|tail -n+2|sed "s/\(.*\)\_.*\_.*$/& s\1/" > $tmp_map
-
 species=`mktemp "species-XXXXX"`
-head -n 1 $genetrees | nw_labels -I - > $species
+head -n 1 $gtre_dmog | nw_labels -I - > $species
 
 map=`mktemp "map-XXXXX"`
 while read line; do
-        #echo $line > $tmp
-        #echo `awk -F 'S' '{print $2;}' $tmp'` 1 $line
         echo $line $line
 done < $species > $map
 
+#call Rscript (the main part is here!)
+Rscript steac_star_njst.r $method $gtre_dmog $map D00 $s_tree
 
-#call Rscript (the main dish is here!)
-Rscript steac_star_njst.r $method $genetrees $map "0_0_0" $s_tree
+#remove dummy outgroup from s_tree
+outtre=`mktemp "tre-XXXXX"`
+nw_prune $s_tree D00 > $outtre
+mv $outtre $s_tree
 
-rm $species $map
+#remove the fixed letter, in our case 'S' (added to genetrees while fixing them)
+sed -i 's/S//g' $s_tree
+
+rm $species $map $gtre_fixed $gtre_dmog
