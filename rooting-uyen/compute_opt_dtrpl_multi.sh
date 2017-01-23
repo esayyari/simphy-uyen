1f4e6e138b386d5185f0e21b86c5605360d3b011
uym2 <ceciliamaitouyen@gmail.com>
2017-01-23 00:52:26 -0800

    compute_opt_dtrpl_multi.sh: save the opt trees as well

--- rooting-uyen/compute_opt_dtrpl_multi.sh
+++ rooting-uyen/compute_opt_dtrpl_multi.sh
@@ -2,15 +2,23 @@
 
 reftrees=$1
 targtrees=$2
+opttrees=$3 #output
+
+if [ -e $opttrees ]; then
+	rm $opttrees
+fi
 
 curr_ref=`mktemp ref-XXXXX`
 curr_targ=`mktemp targ-XXXXX`
+curr_opt=`mktemp opt-XXXXX`
 
 while read ref && read -u 3 targ; do 
 	echo $ref > $curr_ref
 	echo $targ > $curr_targ
-	compute_opt_dtrpl.sh $curr_ref $curr_targ
+	compute_opt_dtrpl.sh $curr_ref $curr_targ $curr_opt
+	printf "\n" >> $opttrees
+	cat $curr_opt >> $opttrees
 done < $reftrees 3< $targtrees
 
-rm $curr_ref $curr_targ
+rm $curr_ref $curr_targ $curr_opt
 
