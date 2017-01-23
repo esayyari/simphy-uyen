7b878a108172d646b0c5c13538ffd189f2db0aa3
uym2 <ceciliamaitouyen@gmail.com>
2017-01-23 00:37:39 -0800

    update compute_opt_dtrpl.sh so that it outputs the opt tree beside the opt dtrpl

--- rooting-uyen/compute_opt_dtrpl.sh
+++ rooting-uyen/compute_opt_dtrpl.sh
@@ -5,17 +5,30 @@
 
 truetree=$1
 esttree=$2
+opttree=$3 # output
 
 info=`mktemp info-XXXXX`
 tmptre=`mktemp tre-XXXXX`
+
+min_dtrpl=`dtrpl_norm.sh $truetree $esttree`
+cp $esttree $opttree
+
 nw_distance -n -mp -sa $esttree > $info
 
 while read line; do
 	node=`echo $line | awk '{print $1}'`
 	x=`echo $line | awk '{print $2/2}'`
 	reroot_at_edge.py -i $esttree -n $node -d $x -o $tmptre
-	dtrpl_norm.sh $truetree $tmptre 
-done < $info | numlist -min
+	dtrpl=`dtrpl_norm.sh $truetree $tmptre`
+	cmp=`python compare_numbers.py $min_dtrpl $dtrpl`
+	if [ "$cmp" -eq 1 ]
+	then
+		min_dtrpl=$dtrpl
+		cp $tmptre $opttree
+	fi
+done < $info
+
+echo $min_dtrpl
 
 rm $info $tmptre
 
