a53c49004faa8aa17274065fc2d9dd1526577551
uym2 <ceciliamaitouyen@gmail.com>
2017-01-23 02:12:43 -0800

    compute_opt_dtrpl.sh: use nw_reroot instead ...

--- rooting-uyen/compute_opt_dtrpl.sh
+++ rooting-uyen/compute_opt_dtrpl.sh
@@ -13,12 +13,14 @@ tmptre=`mktemp tre-XXXXX`
 min_dtrpl=`dtrpl_norm.sh $truetree $esttree`
 cp $esttree $opttree
 
-nw_distance -n -mp -sa $esttree > $info
+#nw_distance -n -mp -sa $esttree > $info
+nw_labels $esttree > $info
 
 while read line; do
 	node=`echo $line | awk '{print $1}'`
-	x=`echo $line | awk '{print $2/2}'`
-	reroot_at_edge.py -i $esttree -n $node -d $x -o $tmptre
+	#x=`echo $line | awk '{print $2/2}'`
+	#reroot_at_edge.py -i $esttree -n $node -d $x -o $tmptre
+	nw_reroot $esttree $node > $tmptre
 	dtrpl=`dtrpl_norm.sh $truetree $tmptre`
 	cmp=`python compare_numbers.py $min_dtrpl $dtrpl`
 	if [ "$cmp" -eq 1 ]
