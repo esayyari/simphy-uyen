c409ad57dbaa5b65e7bbaf577a30f97b18d697c5
uym2 <ceciliamaitouyen@gmail.com>
2017-01-28 18:00:48 -0800

    fix issues

--- rooting-uyen/dendropy_MP.py
+++ rooting-uyen/dendropy_MP.py
@@ -7,5 +7,5 @@ file=argv[1]
 outfile=argv[2]
 
 a_tree = Tree.get_from_path(file,"newick")
-a_tree.reroot_at_midpoint(update_bipartitions=True) 
+a_tree.reroot_at_midpoint() 
 a_tree.write_to_path(outfile,"newick")
