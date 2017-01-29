48303a3d916b2e914ea8c96f263c55c8f5369ba3
uym2 <ceciliamaitouyen@gmail.com>
2017-01-29 08:11:05 -0800

    minor changes

--- rooting-uyen/root_multi.sh
+++ rooting-uyen/root_multi.sh
@@ -8,11 +8,16 @@ outinfo=$4
 [ -e $outtree ] && rm $outtree
 [ -e $outinfo ] && rm $outinfo
 
+temp_in=`mktemp tmp-XXXXX`
+temp_out=`mktemp tmp-XXXXX`
+temp_info=`mktemp tmp-XXXXX`
+
+
 while read l; do 
-	echo $l > temp_in
-	time FastRoot.py -i temp_in -m $method -o temp_out -f temp_info
-	cat temp_out >> $outtree
-	cat temp_info >> $outinfo
-	rm temp_in temp_out temp_info
+	echo $l > $temp_in
+	time FastRoot.py -i $temp_in -m $method -o $temp_out -f $temp_info
+	cat $temp_out >> $outtree
+	cat $temp_info >> $outinfo
+	rm $temp_in $temp_out $temp_info
 done < $intree
 
