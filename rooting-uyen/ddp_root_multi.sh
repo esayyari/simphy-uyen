327b22650e43abf674b5ad2b89c8de5b704211bd
uym2 <ceciliamaitouyen@gmail.com>
2017-01-28 17:00:23 -0800

    timing for ddp

--- rooting-uyen/ddp_root_multi.sh
+++ rooting-uyen/ddp_root_multi.sh
@@ -2,13 +2,13 @@
 
 intree=$1
 outtree=$2
-
+time=$3
 
 [ -e $outtree ] && rm $outtree
 
 while read l; do 
 	echo $l > temp_in
-	time dendropy_MP.py temp_in temp_out
+	{ time dendropy_MP.py temp_in temp_out ; } 2>> $time
 	cat temp_out >> $outtree
 	rm temp_in temp_out 
 done < $intree
