7f804f2f459b06f6439cea00fab6bd81db462ef8
uym2 <ceciliamaitouyen@gmail.com>
2017-03-21 13:57:37 -0700

    a working version of root_multi (without timing)

--- rooting-uyen/root_multi.sh
+++ rooting-uyen/root_multi.sh
@@ -4,7 +4,7 @@ intree=$1
 method=$2
 outtree=$3
 outinfo=$4
-timefile=$5
+#timefile=$5
 
 [ -e $outtree ] && rm $outtree
 [ -e $outinfo ] && rm $outinfo
@@ -16,9 +16,10 @@ temp_info=`mktemp tmp-XXXXX`
 
 while read l; do 
 	echo $l > $temp_in
-	{ time FastRoot.py -i $temp_in -m $method -o $temp_out -f $temp_info ; } 2>> $timefile
+	FastRoot.py -i $temp_in -m $method -o $temp_out -f $temp_info ;
 	cat $temp_out >> $outtree
 	cat $temp_info >> $outinfo
+	rm $temp_in $temp_out $temp_info
 done < $intree
 
 rm $temp_in $temp_out $temp_info
